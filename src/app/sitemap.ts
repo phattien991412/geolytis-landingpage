909f542865b4d146252a826596c2cb152c14f908
Phat <phattien991412@gmail.com>
2025-08-02 18:18:32 +0700

    init

---
+++ src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next'
+import { siteMetadata } from '../../utils'
+
+export const pages = ['']
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const siteUrl = siteMetadata.siteUrl
+
+  const allPages = pages.map((page) => `${siteUrl}/${page}`)
+  const siteMapList = [...allPages].sort()
+
+  return siteMapList.map((s) => ({
+    url: `${s}`,
+    lastModified: new Date().toISOString(),
+    changeFrequency: 'daily',
+    priority: 0.7,
+  }))
+}
